84853bc6af29c8bf70472b0afec6468d87d0a4d7
Rango-lzs <1191472748@qq.com>
2023-04-10 21:39:01 +0800

    add files

---
+++ 2dfem/readfile.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+'''file format
+element : id, eletype node...
+1 1 1 2 3 4
+2 1 2 3 4 5
+node: id, x,y , dof bc (0 for free, -1 for restrainted) 
+1 0.5 0.5 -1 0
+'''
+
+def read_input(file_path = ""):
+    elems = np.loadtxt(file_path+"eles.txt", dtype= int, ndmin= 2)
+    nodes = np.loadtxt(file_path+"nodes.txt",dtype= float,ndmin= 2)
+    loads = np.loadtxt(file_path+"loads.txt",dtype= float, ndmin= 2)
+    mats = np.loadtxt(file_path+"mater.txt", dtype=  float, ndmin= 2)
+    return nodes, elems,loads,mats
+
+
+#nodes, elems,loads,mats = read_input("lzsfem\square-4_elements\\")
+#print(nodes)
+#print(elems)
+#print(loads)
+#print(mats)
